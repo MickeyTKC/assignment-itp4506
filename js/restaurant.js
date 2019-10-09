f59f4b4bcd7c39a898f72c0ec18fcc631d2b287b
Kirsi Mak <54301059+mokiying@users.noreply.github.com>
2019-10-10 03:30:38 +0800

    restaurant
    好餓

---
+++ js/restaurant.js
@@ -0,0 +1,29 @@
+var infobtn = infobtn || document.getElementById("infobtn");
+var info = info || document.getElementById("info");
+var meunsbtn = meunsbtn || document.getElementById("meunsbtn");
+var meuns = meuns || document.getElementById("meuns");
+var branchesbtn = branchesbtn || document.getElementById("branchesbtn");
+var branches = branches || document.getElementById("branches");
+
+infobtn.addEventListener("click", function () {
+    info.style.display = "block";
+    meuns.style.display = "none";
+    branches.style.display = "none";
+});
+
+meunsbtn.addEventListener("click", function () {
+    info.style.display = "none";
+    meuns.style.display = "block";
+    branches.style.display = "none";
+});
+
+branchesbtn.addEventListener("click", function () {
+    info.style.display = "none";
+    meuns.style.display = "none";
+    branches.style.display = "block";
+});
+
+const items = $(".list-group-item");
+const views = $(".view");
+
+items.click(function () { items.removeClass("active"); $(this).addClass("active"); console.log($(this))})
