f4672e64505059ffa7c067e87a44040b122b0e08
MickeyTKC <tt51145505@gmail.com>
2019-10-15 18:44:07 +0800

    edit render.js
    add button[create,set,get]
    edit listGroup to list

--- js/render.js
+++ js/render.js
@@ -8,15 +8,47 @@ const render = {
         }
     },
     button: {
-
-    },
-    listGroup: {
-
+        create:{
+            html:()=>{
+                return `<a href="" class="btn btn-primary">button</a>`;
+            },
+            el:()=>{
+                el = $("<a class='btn btn-primary'>");
+                $(el).attr("href","#");
+                $(el).text("button");
+                return el;
+            },
+        },
+        get:{
+            all:()=>{
+                return $("a.btn");
+            },
+        },
+        set:{
+            text:(el,text)=>{
+                el.innerText=text;
+            },
+        }
     },
     card: {
+        create:{
+            html:()=>{
+                return `
+                `
+            }
+        }
+    },
+    list: {
 
     },
     slider: {
 
     },
+    table:{
+
+    }
 };
+
+const grid = {
+
+}
