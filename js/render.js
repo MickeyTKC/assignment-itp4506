2f8217d944af04765046af0b5e36acc2dee7e13e
MickeyTKC <tt51145505@gmail.com>
2019-10-17 00:05:55 +0800

    add component
    add slider.html
    edit render.js

--- js/render.js
+++ js/render.js
@@ -3,7 +3,6 @@ const render = {
     site: {
         load: () => {
             $("#nav").load("components/nav.html");
-            $("#header").load("components/header.html");
             $("#footer").load("components/footer.html");
         }
     },
@@ -13,7 +12,7 @@ const render = {
                 return `<a href="" class="btn btn-primary">button</a>`;
             },
             el:()=>{
-                el = $("<a class='btn btn-primary'>");
+                const el = $("<a class='btn btn-primary'>");
                 $(el).attr("href","#");
                 $(el).text("button");
                 return el;
@@ -42,7 +41,56 @@ const render = {
 
     },
     slider: {
-
+        create:{
+            html:()=>{
+                return `
+                <div class="slider">
+                <img class="slide" src="image/error.jpg"/>
+                <a class="prev btn btn-primary">‹</a>
+                <a class="next btn btn-primary">›</a>
+                </div>
+                `
+            },
+            el:()=>{
+                const el = $("<div class='slider'>");
+                el.html(`<img class="slide" src="image/error.jpg"/><a class="prev btn btn-primary">‹</a><a class="next btn btn-primary" >›</a>`);
+                return el;
+            }
+        },
+        slides:{
+            cur:0,
+            src:["image/error.jpg"],
+        },
+        get:{
+            all:()=>{
+                return $(".slider");
+            }
+        },
+        set:{
+            images:function(el,slides){
+                let slider = [...$(el)][0];
+                slider.current = 0;
+                slider.slides = slides;
+                const prev= $(el).find(".prev");
+                const next= $(el).find(".next");
+                prev.click(function(){
+                    const slide = $(this).parent().find(".slide");
+                    slider.current--;
+                    if(slider.current<0){
+                        slider.current = slider.slides.length-1
+                    }
+                    slide.attr("src",slider.slides[slider.current])
+                })
+                next.click(function(){
+                    const slide = $(this).parent().find(".slide");
+                    slider.current++;
+                    if(slider.current>=slider.slides.length){
+                        slider.current = 0
+                    }
+                    slide.attr("src",slider.slides[slider.current])
+                })
+            }
+        }
     },
     table:{
 
